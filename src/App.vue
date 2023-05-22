2584fb9a95b5625509f68ad19fc189d06521f8ad
abidino <abidinozd@gmail.com>
2023-05-23 01:00:25 +0300

    add footer

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,29 @@
 
 <template>
   <router-view></router-view>
+  <footer style="padding-top: 20px; margin-top: 30px">
+    <span
+      >Made with <span class="text-lg">🫶🏻</span> by
+      <span>
+        <a
+          href="https://twitter.com/omrumbakitemiz"
+          target="_blank"
+          rel="noopener noreferrer"
+          >@immino
+        </a>
+      </span>
+      <span>and</span>
+      <span>
+        <a
+          href="https://twitter.com/abidinozd"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @abidino
+        </a></span
+      ></span
+    >
+  </footer>
 </template>
 
 <style scoped></style>
